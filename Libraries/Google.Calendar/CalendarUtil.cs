f658fab587cf735494bb4a017660b008afa875c4
blizmanyu <blizman2017@gmail.com>
2019-02-26 15:24:48 -0800

    Added Google Calendar SDK/library

--- Libraries/Google.Calendar/CalendarUtil.cs
+++ Libraries/Google.Calendar/CalendarUtil.cs
@@ -4,9 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Google.Calendar
+namespace Yutaka.Google
 {
-    public class Class1
-    {
-    }
+	public class CalendarUtil
+	{
+		public CalendarUtil() { }
+	}
 }
